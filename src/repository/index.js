b114436b5a984563c611b83a9aa806b17ff1aa18
Tautik Agrahari <tautikagrahari@gmail.com>
2023-05-02 13:45:37 +0530

    added tweet service with hashtag processing

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,5 +1,7 @@
 const { TweetRepository } = require("./tweet-repository");
+const { HastagRepository } = require("./hastag-repository");
 
 module.exports = {
   TweetRepository,
+  HastagRepository,
 };
--- src/service/tweet-service.js
+++ src/service/tweet-service.js
@@ -1,31 +1,53 @@
-const { TweetRepository } = require("../repository/index");
+// Import necessary repositories
+const { TweetRespository, HashtagRepository } = require("../repository/index");
 
+// Create a new TweetService class to handle tweet creation and hashtag association
 class TweetService {
   constructor() {
-    this.tweetRepository = new TweetRepository();
+    // Initialize instances of the repositories
+    this.tweetRepository = new TweetRespository();
+    this.hashtagRepository = new HashtagRepository();
   }
 
+  // Create a new tweet and associate it with the appropriate hashtags
   async create(data) {
-    try {
-      const content = data.content;
+    const content = data.content;
+    const tags = content
+      .match(/#[a-zA-Z0-9_]+/g)
+      .map((tag) => tag.substring(1)); // Extract hashtags from tweet content
 
-      const tags = content.match(/#[a-zA-Z0-9_]+/g); // this regex extracts hashtags
-      tags = tags.map((tag) => tag.slice(1));
-      console.log(tag);
+    // Save the tweet to the database
+    const tweet = await this.tweetRepository.create(data);
 
-      const tweet = await this.tweetRepository.create(data);
-      // todo create hashtags and add here
-      /**
-       * 1.  bulcreate in mongoose
-       * 2. filter title of hashtag based on multiple tags
-       * 3. How to add tweet id inside all the hashtags
-       */
-    } catch (error) {
-      console.log(error);
-    }
+    // Find hashtags that already exist in the database
+    let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+
+    // Get titles of the existing hashtags
+    let titleOfPresenttags = alreadyPresentTags.map((tags) => tags.title);
+
+    // Filter out new hashtags that don't already exist
+    let newTags = tags.filter((tag) => !titleOfPresenttags.includes(tag));
+
+    // Create new hashtag objects for the new tags and associate them with the tweet
+    newTags = newTags.map((tag) => {
+      return { title: tag, tweets: [tweet.id] };
+    });
+
+    // Save the new hashtag objects to the database
+    await this.hashtagRepository.bulkCreate(newTags);
+
+    // Associate the existing hashtag objects with the tweet
+    alreadyPresentTags.forEach((tag) => {
+      tag.tweets.push(tweet.id);
+      tag.save();
+    });
+
+    // Return the created tweet
+    return tweet;
   }
 }
 
+// Export the TweetService class
 module.exports = TweetService;
 
 /*
