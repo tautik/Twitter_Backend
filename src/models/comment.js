9c2fe278f47fc8f9feda21f21e4d64e9f3d61460
Tautik Agrahari <tautikagrahari@gmail.com>
2023-05-04 18:40:00 +0530

    Updated comment model with refpath

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,16 +1,28 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const schema = new mongoose.Schema(
+const commentSchema = new mongoose.Schema(
   {
     content: {
       type: String,
       required: true,
     },
-    userMail: {
+    userId: {
+      ype: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    onModel: {
       type: String,
+      required: true,
+      enum: ["Tweet", "Comment"],
+    },
+    commentable: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      refPath: "onModel",
     },
   },
   { timestamps: true }
 );
 
-export default Comment = mongoose.model("Comment", schema);
+export default Comment = mongoose.model("Comment", commentSchema);
