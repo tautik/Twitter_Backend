ae3d83f3044aee620805146d79f615314292454f
Tautik Agrahari <tautikagrahari@gmail.com>
2023-05-04 18:31:34 +0530

    Added toogleLike API and controller

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import bodyParser from "body-parser";
 import { connect } from "./config/database.js";
 import apiRoutes from "./routes/index.js";
 
-import { UserRespository, TweetRepository } from "./repository/index.js";
-import LikeService from "./services/like-service.js";
-
 const app = express();
 
 const startServer = async () => {
@@ -19,13 +16,6 @@ const startServer = async () => {
     console.log("server started");
     await connect();
     console.log("Mongo db connected");
-
-    const userRepo = new UserRespository();
-    const tweetRepo = new TweetRepository();
-    const tweets = await tweetRepo.getAll(0, 10);
-    const users = await userRepo.getAll();
-    const likeService = new LikeService();
-    await likeService.toggleLike(tweets[0].id, "Tweet", users[0].id);
   });
 };
 startServer();
