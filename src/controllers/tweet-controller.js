618511c875d16f26c99d90c423fd7ae7bd7950a0
Tautik Agrahari <tautik@xerocodee.com>
2023-05-14 11:49:55 +0530

    Mindor changes

--- src/controllers/tweet-controller.js
+++ src/controllers/tweet-controller.js
@@ -1,16 +1,27 @@
 import TweetService from "../services/tweet-service.js";
 
+import upload from "../config/file-upload-s3-config.js";
+
+const singleUploader = upload.single("image");
+
 const tweetService = new TweetService();
 
 export const createTweet = async (req, res) => {
   try {
-    console.log("Entered tweet controller");
-    const response = await tweetService.create(req.body);
-    return res.status(201).json({
-      success: true,
-      message: "Successfully created a new tweet",
-      data: response,
-      err: {},
+    singleUploader(req, res, async function (err, data) {
+      if (err) {
+        return res.status(500).json({ error: err });
+      }
+      console.log("Image url is", req.file);
+      const payload = { ...req.body };
+      payload.image = req.file.location;
+      const response = await tweetService.create(payload);
+      return res.status(201).json({
+        success: true,
+        message: "Successfully created a new tweet",
+        data: response,
+        err: {},
+      });
     });
   } catch (error) {
     return res.status(500).json({
@@ -21,6 +32,7 @@ export const createTweet = async (req, res) => {
     });
   }
 };
+
 export const getTweet = async (req, res) => {
   try {
     const response = await tweetService.get(req.params.id);
